061c72feeb4cd3486d16471c322baec05b12ee44
Andrey <it.personage@gmail.com>
2023-09-20 12:54:24 -0600

    benefits 100%

--- src/Components/Pages/MainPage/Benefits/index.tsx
+++ src/Components/Pages/MainPage/Benefits/index.tsx
@@ -9,21 +9,23 @@ export const Benefits = () => {
       </div>
       <div className={style.benefits_text}>
         <h1>Open-air film experience</h1>
-        <div className={style.benefits_text_item}>
-          <h2>Personal Headphones</h2>
-          <p>Live completely in those movie moments, try to catch them!</p>
-        </div>
-        <div className={style.benefits_text_item}>
-          <h2>City Views</h2>
-          <p>Our locations immerse you in the city skyline at night</p>
-        </div>
-        <div className={style.benefits_text_item}>
-          <h2>Chairs & Cozy Blankets</h2>
-          <p>Lean back and relax under the night sky in the hugs of fluffy blanket</p>
-        </div>
-        <div className={style.benefits_text_item}>
-          <h2>Iconic Movies</h2>
-          <p>Our program is curated with cult films, classics, and new releases</p>
+        <div className={style.benefits_text_items}>
+          <div className={style.benefits_text_item}>
+            <h2>Personal Headphones</h2>
+            <p>Live completely in those movie moments, try to catch them!</p>
+          </div>
+          <div className={style.benefits_text_item}>
+            <h2>City Views</h2>
+            <p>Our locations immerse you in the city skyline at night</p>
+          </div>
+          <div className={style.benefits_text_item}>
+            <h2>Chairs & Cozy Blankets</h2>
+            <p>Lean back and relax under the night sky in the hugs of fluffy blanket</p>
+          </div>
+          <div className={style.benefits_text_item}>
+            <h2>Iconic Movies</h2>
+            <p>Our program is curated with cult films, classics, and new releases</p>
+          </div>
         </div>
       </div>
     </div>
