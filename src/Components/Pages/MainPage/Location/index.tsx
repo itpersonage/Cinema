38b890d42fdec5ab42e56ba8ec312c4a36df5fbe
lebmed9 <93411240+lebmed9@users.noreply.github.com>
2023-10-04 10:03:55 -0600

    location 100%

--- src/Components/Pages/MainPage/Location/index.tsx
+++ src/Components/Pages/MainPage/Location/index.tsx
@@ -45,6 +45,31 @@ export const Location = () => {
             <button type="button">Choose location</button>
             <img src="./images/place-1.jpg" alt="place-1" />
           </div>
+          <div className={style.location_item_text}>
+            <div className={style.location_item_text_top}>
+              <h2>Cinema Club El Segundo</h2>
+              <div className={style.text_top_rating}>
+                <svg
+                  width="16"
+                  height="15"
+                  viewBox="0 0 16 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z"
+                    fill="white"
+                  />
+                </svg>
+                <p>4.6</p>
+              </div>
+            </div>
+            <div className={style.location_item_text_bottom}>
+              <h2>Los Angeles</h2>
+              <div className={style.text_bottom_price}>
+                  <p><span>From $25</span>/person</p>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className={style.location_item}>
@@ -52,11 +77,61 @@ export const Location = () => {
             <button type="button">Choose location</button>
             <img src="./images/place-2.jpg" alt="place-2" />
           </div>
+          <div className={style.location_item_text}>
+            <div className={style.location_item_text_top}>
+              <h2>The Drive-In Off Navigation</h2>
+              <div className={style.text_top_rating}>
+                <svg
+                  width="16"
+                  height="15"
+                  viewBox="0 0 16 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z"
+                    fill="white"
+                  />
+                </svg>
+                <p>4.8</p>
+              </div>
+            </div>
+            <div className={style.location_item_text_bottom}>
+              <h2>Houston</h2>
+              <div className={style.text_bottom_price}>
+                  <p><span>From $22</span>/person</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className={style.location_item}>
           <div className={style.location_item_cover}>
             <button type="button">Choose location</button>
             <img src="./images/place-3.jpg" alt="place-3" />
+          </div>
+          <div className={style.location_item_text}>
+            <div className={style.location_item_text_top}>
+              <h2>Cinema Club Embracadero</h2>
+              <div className={style.text_top_rating}>
+                <svg
+                  width="16"
+                  height="15"
+                  viewBox="0 0 16 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z"
+                    fill="white"
+                  />
+                </svg>
+                <p>4.2</p>
+              </div>
+            </div>
+            <div className={style.location_item_text_bottom}>
+              <h2>San Diego</h2>
+              <div className={style.text_bottom_price}>
+                  <p><span>From $25</span>/person</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
